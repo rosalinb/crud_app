9e797ae43618a01f201912339a1a484364643997
rosalinb <rosalinbiswal009@gmail.com>
2022-05-19 14:55:12 +1000

    login route and erb file added.

--- main.rb
+++ main.rb
@@ -5,7 +5,18 @@ get '/' do
   erb :'home/index'
 end
 
+enable :sessions
+
 require './controllers/properties_controller'
+require './controllers/sessions_controller'
+require './controllers/users_controller'
+
+
+
+require './models/user'
+require './models/properties'
+
+require './helpers/sessions_helper'
 
 
 
