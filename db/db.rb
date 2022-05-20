f4a1343469e6acc80508d05cfdd6871e89ec8330
rosalinb <rosalinbiswal009@gmail.com>
2022-05-20 10:50:19 +1000

    sign_up

--- db/db.rb
+++ db/db.rb
@@ -2,9 +2,9 @@ require 'pg'
 
 def run_sql(sql_code, params = [])
 
-  db = PG.connect(dbname: 'property_finder')
+  db = PG.connect(ENV['DATABASE_URL'] || {dbname: 'property_finder'})
   data = db.exec_params(sql_code, params)
   db.close
   data
-  
+
 end 
