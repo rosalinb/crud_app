de8072dc95331c40a646c5883d03faddb4cb346d
rosalinb <rosalinbiswal009@gmail.com>
2022-05-20 14:46:58 +1000

    log_in and log_out functionality with user_specfic added

--- db/schema.sql
+++ db/schema.sql
@@ -9,3 +9,20 @@ id SERIAL PRIMARY KEY,
     password_digest TEXT,
     user_type TEXT
 );
+
+CREATE TABLE properties(
+    id SERIAL PRIMARY KEY,
+    image_url TEXT,
+    house_number TEXT,
+    street_name TEXT,
+    postcode INTEGER,
+    city TEXT,
+    state TEXT,
+    country TEXT,
+    property_type TEXT,
+    number_of_bedrooms INTEGER,
+    number_of_bathrooms INTEGER,
+    number_of_parkings INTEGER,
+    price REAL
+);
+
