0164cf302b534ce87ae0de10ab18ca4d9b9dc9eb
rosalinb <rosalinbiswal009@gmail.com>
2022-05-22 00:11:31 +1000

    error's fixed

--- controllers/properties_controller.rb
+++ controllers/properties_controller.rb
@@ -28,9 +28,9 @@ post '/create_properties' do
   suburb = params['suburb']
   state = params['state']
   property_type = params['property_type']
-  number_of_bedrooms = params['number_of_bedrooms']
-  number_of_bathrooms = params['number_of_bathrooms']
-  number_of_parkings = params['number_of_parkings']
+  number_of_bedrooms = params['num_of_bedrooms']
+  number_of_bathrooms = params['num_of_bathrooms']
+  number_of_parkings = params['num_of_parkings']
   price = params['price']
   created_by = session['user_id']
 
@@ -77,4 +77,24 @@ delete '/properties/:id' do
 
     redirect '/properties/agent_dashboard'
   
+  end
+
+
+  post '/properties/search' do
+    search_text = params['search_text']
+    search_result = search_properties(search_text)
+
+    if search_text == ''
+      property_listings = all_properties()
+
+      erb :'/properties/listings', locals: {
+        property_listings: property_listings
+      }
+    else 
+      erb :'/properties/listings', locals: {
+        property_listings: search_result
+      }
+    end
+
+    
   end
--- models/properties.rb
+++ models/properties.rb
@@ -6,6 +6,10 @@ def my_properties(created_by)
     run_sql('SELECT * FROM properties WHERE created_by = $1', [created_by])
 end 
 
+def search_properties(search_text)
+    run_sql('SELECT * FROM properties WHERE upper(suburb) like $1', [search_text.upcase])
+end 
+
 def create_property(image_url,ad_line,house_number,street_name,postcode,suburb,state,property_type,number_of_bedrooms,number_of_bathrooms,number_of_parkings,price,created_by)
     run_sql("INSERT INTO properties(image_url,ad_line,house_number,street_name,postcode,suburb,state,property_type,number_of_bedrooms,number_of_bathrooms,number_of_parkings,price,created_by) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)", [image_url,ad_line,house_number,street_name,postcode,suburb,state,property_type,number_of_bedrooms,number_of_bathrooms,number_of_parkings,price,created_by])
 end
